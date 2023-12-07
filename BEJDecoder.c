589a2a1a299a5c11079269f5dc5347b0decf830f
Nazarii-Bodnar <adresa.nazar.bodnar@gmail.com>
2023-12-07 15:23:11 +0200

    fix tupleSeparator

--- BEJDecoder.c
+++ BEJDecoder.c
@@ -6,7 +6,8 @@
 //TODO: unIntHandler not writing variables
 void unIntHandler(FILE* source, unsigned int* output){
     unsigned char length = fgetc(source);
-    fread_s(output, sizeof(unsigned int), length, 1, source);
+    int check;
+    check = fread_s(output, sizeof(unsigned int), length, 1, source);
 }
 
 void copyArray(char* source, char* dest, int length, int start){
@@ -20,27 +21,27 @@ bejTuple* separateInTuples(FILE* source, int startPoint, unsigned int maxCount){
     if(startPoint){
         fseek(source, startPoint, SEEK_SET);
     }
-    int count = 1;
+    int index = 0;
     unsigned int childCount = 0;
-    while(!feof(source) && maxCount && count <= maxCount){
-        result = (bejTuple*)realloc(result, count*sizeof(bejTuple));
-        result[count].sequence = 0;
-        unIntHandler(source, &result[count].sequence);
+    while(!feof(source) && maxCount && index < maxCount){
+        result = (bejTuple*)realloc(result, (index+1)*sizeof(bejTuple));
+        result[index].sequence = 0;
+        unIntHandler(source, &result[index].sequence);
         unsigned char formatFlags = fgetc(source);
-        result[count].format = formatFlags >> 4;
-        result[count].flags = formatFlags & 0x0F;//TODO: flag is to big
-        result[count].length = 0;
-        unIntHandler(source, &result[count].length);
-        if(result[count].format == 0x00 || result[count].format == 0x01){
+        result[index].format = formatFlags >> 4;
+        result[index].flags = formatFlags & 0x0F;
+        result[index].length = 0;
+        unIntHandler(source, &result[index].length);
+        if(result[index].format == 0x00 || result[index].format == 0x01){
             unIntHandler(source, &childCount);
-            result[count].children = separateInTuples(source, 0, childCount);
+            result[index].children = separateInTuples(source, 0, childCount);
         }
         else{
-            result[count].children = NULL;
-            result[count].value = calloc(result[count].length, sizeof(char));
-            fread_s(result[count].value, result[count].length, result[count].length, 1, source);
+            result[index].children = NULL;
+            result[index].value = calloc(result[index].length, sizeof(char));
+            fread_s(result[index].value, result[index].length, result[index].length, 1, source);
         }
-        count++;
+        index++;
     }
 
     return result;
